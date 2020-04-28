209a628989f524acf10354a8e94cceb8e7c446e9
Bala Luguja <bluguja@gmail.com>
2020-04-28 06:41:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ end
         song_name = file_name.split(" - ")[1]
         artist_name = file_name.split(" - ")[0]
         song = Song.new(song_name)
-        #song.name = song_name      # what id this code supp
+        song.name = song_name      # what id this code supp
         song.artist_name = artist_name
         song
     end
