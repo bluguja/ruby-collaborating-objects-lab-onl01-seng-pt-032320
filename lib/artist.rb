196ba16d4bf5d68e6057c27bfa7d99d26548f24b
Bala Luguja <bluguja@gmail.com>
2020-04-28 06:22:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -46,11 +46,7 @@ class Artist
   end
     
  
-  def self.song_count
-  Song.all.length
-  end
-  
    def print_songs
-     self.songs.each {|song| puts song.name} 
+     self.songs. {|song| puts song.name} 
    end
 end
